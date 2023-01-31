50094578a4583a9e7ac53e2af00de00d7790b6de
PrabirKalwani <70889682+PrabirKalwani@users.noreply.github.com>
2023-02-01 03:13:48 +0530

    Added Video in AboutTq Page
    ->please add content

--- src/pages/ParentContainer.js
+++ src/pages/ParentContainer.js
@@ -2,13 +2,14 @@ import React from "react";
 import logo1 from "../assets/img1.png"
 import tqbg from "../assets/bg4.jpg"
 import Animate_cursor from '../components/animate_cursor'
-
+import videobg from '../assets/svid.mp4'
 const ParentContainer = () => {
   return (
     <>
     
             <Animate_cursor />
     <div>
+<<<<<<< HEAD
 
     <div className="parentContainer scroll-c">
         <div className="abouttext">
@@ -33,6 +34,21 @@ const ParentContainer = () => {
         <br></br>
         <img src={logo1} className="aboutimage" />
       </div>
+=======
+    <div className="main-tq">
+      <video src={videobg} autoPlay loop muted/>
+    </div>
+    <div className="Content-main-tq">
+      <h1>About Taqneeq</h1>
+      <p>lorem</p>
+    </div>
+   
+
+
+
+
+
+>>>>>>> 1eb4c21 (Added Video in AboutTq Page)
       </div></>
   );
 };
