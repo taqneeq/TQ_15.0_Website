d9c8a83dde84ee2edf4dc5a454e8f420f38690cb
PrabirKalwani <70889682+PrabirKalwani@users.noreply.github.com>
2023-02-03 14:51:49 +0530

    Added images to tech and collab page

--- src/pages/collab.js
+++ src/pages/collab.js
@@ -17,91 +17,100 @@ export default function Collab()
 
 
   
-  <div class="final card-collab">
+  <div class="final-1 card-collab">
     <div class="wrapper-collab">
       <div class="date-collab">
         <span class="day-collab">6</span>
         <span class="month-collab">Feb</span>
         <span class="year-collab">2023</span>
       </div>
-      <ul class="menu-content-collab">
+      {/* <ul class="menu-content-collab">
           <li><a href="#"><i class="fab fa-github"></i></a></li>
           <li><a href="#"><i class="fab fa-html5"></i></a></li>
           <li><a href="#"><i class="fab fa-css3-alt"></i></a></li>
           <li><a href="#"><i class="fab fa-js"></i></a></li>
-      </ul>
+      </ul> */}
       <div class="data-collab">
         <div class="content-text">
           <span class="type-collab">TQxCollab</span>
           <h1 class="title-collab"><a href="#">Cadathon</a></h1>
-          <p class="text-collab">The competition will commence on 6th February, 2023, at 8am. The competition will come to an end on 7th February, 2023, at 11:59pm.</p>
+          <p class="text-collab">The competition will commence on 6th February, 2023, at 8am. </p>
+          <button class="ebtn">Register</button>
+
         </div>
       </div>
     </div>
   </div>
-  <div class="final card-collab">
+
+  <div class="final-2 card-collab">
     <div class="wrapper-collab">
       <div class="date-collab">
         <span class="day-collab">7</span>
         <span class="month-collab">Feb</span>
         <span class="year-collab">2023</span>
       </div>
-      <ul class="menu-content-collab">
+      {/* <ul class="menu-content-collab">
           <li><a href="#"><i class="fab fa-github"></i></a></li>
           <li><a href="#"><i class="fab fa-html5"></i></a></li>
           <li><a href="#"><i class="fab fa-css3-alt"></i></a></li>
           <li><a href="#"><i class="fab fa-js"></i></a></li>
-      </ul>
+      </ul> */}
       <div class="data-collab">
         <div class="content-text">
           <span class="type-collab">TQxCollab</span>
           <h1 class="title-collab"><a href="#">Technical Quiz</a></h1>
           <p class="text-collab">The competition will commence on 7th February, 2023, 10am onwards.</p>
+          <button class="ebtn">Register</button>
+
         </div>
       </div>
     </div>
   </div>
-  <div class="final card-collab">
+  <div class="final-3 card-collab">
     <div class="wrapper-collab">
       <div class="date-collab">
         <span class="day-collab">7</span>
         <span class="month-collab">Feb</span>
         <span class="year-collab">2023</span>
       </div>
-      <ul class="menu-content-collab">
+      {/* <ul class="menu-content-collab">
           <li><a href="#"><i class="fab fa-github"></i></a></li>
           <li><a href="#"><i class="fab fa-html5"></i></a></li>
           <li><a href="#"><i class="fab fa-css3-alt"></i></a></li>
           <li><a href="#"><i class="fab fa-js"></i></a></li>
-      </ul>
+      </ul> */}
       
       <div class="data-collab">
         <div class="content-text">
           <span class="type-collab">TQxCollab</span>
           <h1 class="title-collab"><a href="#">Technical Debate</a></h1>
           <p class="text-collab">The Debate Competition will commence on 7th February, 2023, 3pm onwards.</p>
+          <button class="ebtn">Register</button>
+
         </div>
       </div>
     </div>
   </div>
-  <div class="final card-collab">
+  <div class="final-4 card-collab">
     <div class="wrapper-collab">
       <div class="date-collab">
         <span class="day-collab">8</span>
         <span class="month-collab">Feb</span>
         <span class="year-collab">2023</span>
       </div>
-      <ul class="menu-content-collab">
+      {/* <ul class="menu-content-collab">
           <li><a href="#"><i class="fab fa-github"></i></a></li>
           <li><a href="#"><i class="fab fa-html5"></i></a></li>
           <li><a href="#"><i class="fab fa-css3-alt"></i></a></li>
           <li><a href="#"><i class="fab fa-js"></i></a></li>
-      </ul>
+      </ul> */}
       <div class="data-collab">
         <div class="content-text">
           <span class="type-collab">TQxCollab</span>
           <h1 class="title-collab"><a href="#">Forging Your Way</a></h1>
           <p class="text-collab">The event will commence on 8th February, 2023, 10am onwards.</p>
+          <button class="ebtn">Register</button>
+
         </div>
       </div>
     </div>
