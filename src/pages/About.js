d9c72e596f38156720d5c09ad6cdcdaae727325b
Darsh Mashru <mashrudarsh31@gmail.com>
2023-02-03 12:08:12 +0530

    Revert "Fixed Navbar and About Tq page"
    This reverts commit 9e6601b7c25d580354f55a6141ef9d35e1f85fbb.

--- src/pages/About.js
+++ src/pages/About.js
@@ -20,21 +20,21 @@ const About = () =>
                                 <h1 className="text-center display-5">ABOUT TAQNEEQ</h1>
 
                             </div>
-                            <div className="mt-5 text-center fs-5 heynigga">
-                                <p className="about-tq-text">Over the years, Taqneeq has turned out to be an exceptional tech fest with 3 fun-filled & power-packed days in a row,
+                            <div className="mt-5 text-center fs-5">
+                                <p>Over the years, Taqneeq has turned out to be an exceptional tech fest with 3 fun-filled & power-packed days in a row,
                                     it has illuminated each & every side of technology, with immense successes in organising remarkable events such as
                                     The Talk, Dare2Compete, Internship Fair, Game Development Workshop, Light Reaction, to name a few.</p>
 
                                 <br></br>
 
-                                <p className="about-tq-text">
+                                <p>
                                     This year, as we reminisce about its previous versions, we seek to make it even more imposing & memorable for
                                     all of us. Episode 15 of Taqneeq encircles around
                                 </p>
 
                                 <br></br>
 
-                                <p className="about-tq-text">
+                                <p>
                                     What meets the eye is not necessarily as it seems to be. TQ 15.0 is back with even more energy & enthusiasm!
                                     Times are busy and calendars full — save the dates!
                                 </p>
