0c73f7e17e973b0113d8c04bbba4bb6817d775dd
PrabirKalwani <70889682+PrabirKalwani@users.noreply.github.com>
2023-02-05 00:54:48 +0530

    added modals
    will at rest changes on 6feb

--- src/pages/Workshop.js
+++ src/pages/Workshop.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import videobg from '../assets/svid2.mp4'
 
-
 export default function Workshop()
 {
 
@@ -91,7 +90,7 @@ export default function Workshop()
                         <i className="fab fa-instagram" />
                         <i className="fab fa-github" />
                     </div>
-                    <button onClick={drone}></button>
+                    <button data-bs-toggle="modal" data-bs-target="#Modal-1" ></button>
                 </div>
 
 
@@ -145,7 +144,7 @@ export default function Workshop()
                         </i>
 
                     </div>
-                    <button onClick={motorsport}></button>
+                    <button data-bs-toggle="modal" data-bs-target="#Modal-2"></button>
                 </div>
 
                 <div className="wcard wcard3">
@@ -251,11 +250,52 @@ export default function Workshop()
                         <i className="fab fa-instagram" />
                         <i className="fab fa-github" />
                     </div>
-                    <button onClick={gamedev}></button>
+                    <button ></button>
                 </div>
 
 
+{/* modal's For Workshop */}
+<div class="modal fade" id="Modal-1" tabindex="-1" aria-labelledby="Modal-1Label" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <div class="modal-title" id="Modal-1Label">
+            <img class="workshop-modal-image" src='https://images.unsplash.com/photo-1473968512647-3e447244af8f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80' alt="drone" />
+        </div>
+      </div>
+      <div class="modal-body">
+        <h1>DroneSystem Coretech</h1>
+      A FREE hands-an session where participants will learn about drone types, core technology, flight principles and control of drones followed by actual assembly, configuration and flight of a simple quadcopter.
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <a  class="btn btn-primary" href="https://ugf11b5z0iy.typeform.com/to/m5XyietS">Register</a>
+      </div>
+    </div>
+  </div>
+</div>
 
+
+
+<div class="modal fade" id="Modal-2" tabindex="-1" aria-labelledby="Modal-2Label" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <div class="modal-title" id="Modal-2Label">
+            <img class="workshop-modal-image" src='https://images.unsplash.com/photo-1485906441524-ed91f8315b4a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80' alt="motorsport" />
+        </div>
+      </div>
+      <div class="modal-body">
+        <h1>Unlearning Motorsport</h1>
+        Here's Jamie Shaw, the brand ambassador of Motorsport, to share his knowledge about racing.Join us at the seminar & get your adrenaline pumping!
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <a  class="btn btn-primary" href="https://ugf11b5z0iy.typeform.com/to/a7eYqxQJ">Register</a>
+      </div>
+    </div>
+  </div>
+</div>
                 </div>
                 </div>
         </>
