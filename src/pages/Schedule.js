8d115830a46f43d0600eb0c899a2e04cfe3ac565
PrabirKalwani <70889682+PrabirKalwani@users.noreply.github.com>
2023-02-05 22:22:57 +0530

    removed schedule modal

--- src/pages/Schedule.js
+++ src/pages/Schedule.js
@@ -1098,27 +1098,7 @@ racing presentations!</p>
                     </div>
                 </div>
             </div>
-            {/* modals for schedule page */}
-            <div class="modal fade" id="schedule1" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog modal-dialog-centered">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <img class="card-img-top" src="https://images.unsplash.com/photo-1675141194800-ae6f2f729ed9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="Card image cap"></img>
-                        </div>
-                        <div class="modal-body">
-                            <h1 className='modal-texts'>lorem</h1>
-                            <p className='modal-texts' >lorem</p>
-
-                        </div>
-                        <div class="modal-footer">
-                            <a type="button" class="btn btn-primary" href="#">Register</a>
-                            <button type="button" class="btn btn-secondary" data-dismiss="schedule1">Close</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-
+            
 
         </>
     )
